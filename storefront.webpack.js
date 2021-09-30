7e7fc39e574f6dbef3b3bab385d1dc1393fa539c
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-09-30 12:05:34 -0300

    feat: update atacado

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -5,7 +5,9 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
-      './html/ProductGallery.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductGallery.html')
+      './html/ProductGallery.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductGallery.html'),
+      './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/html/APrices.html'),
+      './js/APrices.js': path.resolve(__dirname, 'template/js/custom-js/js/APrices.js')
     }
   }
 })
